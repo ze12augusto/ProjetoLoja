9aec0dac16028e767498f37117989d8a9afe5f6d
Luiz Fernando <luizfernando_alves78@hotmail.com>
2015-03-11 19:59:13 -0300

    Formulario de Fornecedor

--- ProjetoLojaDesktop/ProjetoLojaDesktop/Program.cs
+++ ProjetoLojaDesktop/ProjetoLojaDesktop/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace ProjetoLojaDesktop
+namespace cad_fornecedor
 {
     static class Program
     {
@@ -15,7 +15,7 @@ namespace ProjetoLojaDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormFornecedor());
         }
     }
 }
